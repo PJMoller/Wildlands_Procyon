6bc333674f79f0e86a41ccb62a390a38f6c5ef0b
neszm <neszmark@gmail.com>
2025-09-30 14:08:49 +0200

    Added simple Kfold cross validation

--- tests/model_testing/model_training.py
+++ tests/model_testing/model_training.py
@@ -1,11 +1,10 @@
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_validate, KFold
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, make_scorer
 import pandas as pd
 
 # loading the processed data
 processed_df = pd.read_csv("../../data/processed/processed_merge.csv")
-
 
 # getting the data ready
 X = processed_df.drop(columns=["ticket_num"])
@@ -17,13 +16,23 @@ model = RandomForestRegressor(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 
-
 mae = mean_absolute_error(y_test, y_pred)
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 #print(y_test.values)
 #print(y_pred)
 print(f"MAE: {mae}, MSE: {mse}, R2: {r2}")
-# MAE: 32.28782051282052, MSE: 9095.703798717948, R2: 0.9374264233080393 sheesh rly nice scores
+
+# crossval
+scoring = {"MAE": make_scorer(mean_absolute_error),"MSE": make_scorer(mean_squared_error),"R2": "r2"}
+kf = KFold(n_splits=5, shuffle=True, random_state=42)
+cv_results = cross_validate(model, X, y, cv=kf, scoring=scoring)
+
+print("CV MAE:", cv_results["test_MAE"])
+print("CV MSE:", cv_results["test_MSE"])
+print("CV R2:", cv_results["test_R2"])
+print("Mean CV MAE:", cv_results["test_MAE"].mean())
+print("Mean CV MSE:", cv_results["test_MSE"].mean())
+print("Mean CV R2:", cv_results["test_R2"].mean())
 
 # save the model to connect to the website later
