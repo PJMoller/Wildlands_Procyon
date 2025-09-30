090de3c693705e07b475a49fe5131624b7711da3
iLootYou <leon.kremer@student.nhlstenden.com>
2025-09-30 21:32:23 +0200

    Polynomial regressions (for now)

--- tests/model_testing/model_training.py
+++ tests/model_testing/model_training.py
@@ -1,8 +1,9 @@
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.pipeline import Pipeline
 import pandas as pd
 import pickle
 
@@ -13,7 +14,7 @@ processed_df = pd.read_csv("../../data/processed/processed_merge.csv")
 X = processed_df.drop(columns=["ticket_num"])
 y = processed_df["ticket_num"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
+"""
 # RF Regressor with hyperparameter tuning
 model = RandomForestRegressor(random_state=42)
 
@@ -40,13 +41,31 @@ r2 = r2_score(y_test, y_pred)
 #print(y_pred)
 print(f"RF MAE: {mae}, MSE: {mse}, R2: {r2}")
 """
+
 # Polynomial Regression
-poly = PolynomialFeatures(degree=2)
-x_poly = poly.fit_transform(X_train)
-model1 = LinearRegression()
-model1.fit(x_poly, y_train)
-x_test_poly = poly.transform(X_test)
-y_pred = model1.predict(x_test_poly)
+
+# A pipeline that first creates polynomial features then applies linear regression
+model1_pipe = Pipeline([
+    ('poly', PolynomialFeatures()),
+    ('ridge', Ridge()),
+])
+
+# After some experimentation, these parameters were found to be optimal (my VSC didnt like the other parameters)
+param_grid = {
+    'poly__degree': [2],
+    'ridge__alpha': [10.0]
+}
+
+
+grid_search = GridSearchCV(estimator=model1_pipe,param_grid=param_grid,cv=5,
+                           scoring="neg_mean_absolute_error")
+
+grid_search.fit(X_train, y_train)
+
+print("Best parameters found:", grid_search.best_params_)
+
+best_model = grid_search.best_estimator_
+y_pred = best_model.predict(X_test)
 
 
 mae = mean_absolute_error(y_test, y_pred)
@@ -58,6 +77,5 @@ print(f"Poly MAE: {mae}, MSE: {mse}, R2: {r2}")
 
 # save the model to connect to the website later
 
-with open("../../data/processed/model.pkl", "wb") as f:
-    pickle.dump(model, f)
-"""
+#with open("../../data/processed/model.pkl", "wb") as f:
+#    pickle.dump(model, f)
