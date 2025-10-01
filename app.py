33c878640e2f2a5e37f1515d7690228a51b531d9
Christy-kwakkel <christy.kwakkel@student.nhlstenden.com>
2025-10-01 11:04:34 +0200

    added the openmeteo weather api forecast for emmen for 7 days with the same data as wildlands uses

--- app.py
+++ app.py
@@ -4,11 +4,55 @@ import numpy as np
 import sqlite3
 from datetime import datetime
 import pandas as pd
+import openmeteo_requests
+import requests_cache
+from retry_requests import retry
+
+# Open-Meteo API setup and retry if you get an error
+cache_session = requests_cache.CachedSession(".cache", expire_after = 3600)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+# setting up location and parameters
+url = "https://api.open-meteo.com/v1/forecast"
+params = {
+    "latitude": 52.7862,
+    "longitude": 6.8917,
+    "hourly": ["temperature_2m", "precipitation", "rain"] 
+}
+responses = openmeteo.weather_api(url, params=params)
+
+# processing the location
+response = responses[0]
+print(f"Coordinates: {response.Latitude()}°N {response.Longitude()}°E")
+print(f"Elevation: {response.Elevation()} m asl")
+print(f"Timezone difference to GMT+0: {response.UtcOffsetSeconds()}s")
+
+# Process hourly data
+hourly = response.Hourly()
+hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+hourly_precipitation = hourly.Variables(1).ValuesAsNumpy()
+hourly_rain = hourly.Variables(2).ValuesAsNumpy()
+
+hourly_data = {"date": pd.date_range(
+	start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+	end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+	freq = pd.Timedelta(seconds = hourly.Interval()),
+	inclusive = "left"
+)}
+
+hourly_data["temperature_2m"] = hourly_temperature_2m
+hourly_data["precipitation"] = hourly_precipitation
+hourly_data["rain"] = hourly_rain
+
+hourly_dataframe = pd.DataFrame(data = hourly_data)
+print("\nHourly data\n", hourly_dataframe)
+
 
 app = Flask(__name__)
 
 # Loading the ML model
-model = joblib.load("model.pkl")
+model = joblib.load("data/processed/model.pkl")
 
 # Database file
 DB_FILE = "data/database/database.db"
@@ -19,32 +63,32 @@ for loop for looking into 3 cattegorries
 2: start with ticket_
 3: holiday/special day
 """
-# loading the processed data
-processed_df = pd.read_csv("data/processed/processed_merge.csv")
+# # loading the processed data
+# processed_df = pd.read_csv("data/processed/processed_merge.csv")
 
-headers = list(processed_df.columns)
-#print(headers)
+# headers = list(processed_df.columns)
+# #print(headers)
 
-for col in processed_df.columns:
-    #print(col)
+# for col in processed_df.columns:
+#     #print(col)
 
-    if col == "temperature" or col == "rain" or col == "percipitation":
-        print("API")
-    elif col.startswith("ticket_"):
-        print("ticket_")
-    else:
-        if col.startswith("NLNoord"):
-            print("Noord")
-        elif col.startswith("NLMidden"):
-            print("Midden")
-        elif col.startswith("NLZuid"):
-            print("Zuid")
-        elif col.startswith("Niedersachsen"):
-            print("Niedersachsen")
-        elif col.startswith("Nordrhein-Westfalen"):
-            print("Nordrhein-Westfalen")
-        else:
-            print("overig")
+#     if col == "temperature" or col == "rain" or col == "percipitation":
+#         print("API")
+#     elif col.startswith("ticket_"):
+#         print("ticket_")
+#     else:
+#         if col.startswith("NLNoord"):
+#             print("Noord")
+#         elif col.startswith("NLMidden"):
+#             print("Midden")
+#         elif col.startswith("NLZuid"):
+#             print("Zuid")
+#         elif col.startswith("Niedersachsen"):
+#             print("Niedersachsen")
+#         elif col.startswith("Nordrhein-Westfalen"):
+#             print("Nordrhein-Westfalen")
+#         else:
+#             print("overig")
     
 
 
