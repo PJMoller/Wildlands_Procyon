866f8a522998a26b5f0133d88ce2bca5fc3d74df
Peter Möller <peter.moller@student.nhlstenden.com>
2025-10-09 13:45:42 +0200

    added folder

---
+++ tests/visulization/visulization_test.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import plotly.express as px
+
+# Read CSV (note the semicolon separator)
+df = pd.read_csv("./data/raw/visitordaily.csv", sep=";")
+
+# Create a line chart of entrances per day per group
+fig = px.line(
+    df,
+    x="Date",
+    y="NumberOfUsedEntrances",
+    color="Description",
+    title="Entrances Over Time",
+    markers=True
+)
+
+# Export chart as HTML
+fig.write_html("./tests/visulization/chart.html")
